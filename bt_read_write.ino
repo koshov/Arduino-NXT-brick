473409d4b24b9eb49cf1bc80f03daa6229e0de3d
Georgi Koshov <koshov@gmail.com>
2013-02-28 23:10:49 +0000

    Shit almost fixed

--- bt_read_write.ino
+++ bt_read_write.ino
@@ -8,16 +8,17 @@ int backM1 = 6;    // Right motor white wire
 void setup() {
     Wire.begin();
     Serial.begin(9600); 
+    Serial.println("Hello!");
+
     pinMode(frontM0, OUTPUT);
     pinMode(frontM1, OUTPUT);
     pinMode(backM0, OUTPUT);
     pinMode(backM1, OUTPUT);
-    Serial.println("Hello!");
 }
 
 void loop() {
     if (Serial.available()){
-        int ctrlVals[5];    // Array to store control values
+        byte ctrlVals[5];    // Array to store control values
         int i = 0;          // Counter for control package validation
         delay(5);           // Timeout for serial buffer consistency
         while (Serial.available() && i<5){
@@ -26,11 +27,11 @@ void loop() {
         }
         if (i==5) {         // Control package is fine
             for (int i=0; i<5; i++){
-                int direction_current = bitRead(ctrlVals[i], 7);
-                Serial.print(ctrlVals[i]);
-                Serial.print(' - ');
-                Serial.print(direction_current);
-                Serial.print('; ');
+                Serial.print(bitRead(ctrlVals[i], 7));
+                Serial.print(" - ");
+                bitClear(ctrlVals[i], 7);
+                Serial.print(ctrlVals[i]<<1);
+                Serial.print("; ");
               }
             Serial.println();
             processVals(ctrlVals);
@@ -38,6 +39,7 @@ void loop() {
             Wire.write(ctrlVals[0]);
             Wire.write(ctrlVals[1]);
             Wire.endTransmission();
+            Serial.println("Tuk!");
         } else if (i>5){
             Serial.println("Control signal larger than 5 bytes");
         } else {
@@ -46,21 +48,27 @@ void loop() {
     }
 }
 
-void processVals(int ctrlVals[5]){
-    int direction_back = bitRead(ctrlVals[2], 7);
+void processVals(byte ctrlVals[5]){
     int direction_front = bitRead(ctrlVals[3], 7);
+    bitClear(ctrlVals[3], 7);
+    byte front = ctrlVals[3]<<1;
+
+    int direction_back = bitRead(ctrlVals[2], 7);
+    bitClear(ctrlVals[2], 7);
+    byte back = ctrlVals[2]<<1;
+
     if (direction_front) {    // Move forward
         analogWrite(frontM1, 0);
-        analogWrite(frontM0, ctrlVals[3]<<1);
+        analogWrite(frontM0, front);
     } else {
         analogWrite(frontM0, 0);
-        analogWrite(frontM1, ctrlVals[3]<<1);
+        analogWrite(frontM1, front);
     }
     if (direction_back) {    // Move forward
         analogWrite(backM0, 0);
-        analogWrite(backM1, ctrlVals[2]<<1);
+        analogWrite(backM1, back);
     } else {
         analogWrite(backM1, 0);
-        analogWrite(backM0, ctrlVals[2]<<1);
+        analogWrite(backM0, back);
     }
 }
--- second_arduino/second_arduino.ino
+++ second_arduino/second_arduino.ino
@@ -4,61 +4,64 @@ int leftM0 = 11;    // Left motor black wire
 int leftM1 = 10;    // Left motor white wire
 int rightM0 = 5;    // Right motor black wire
 int rightM1 = 6;    // Right motor white wire
-int left = 0;
-int right = 0;
 
 void setup()
 {
     Wire.begin(4);
     Wire.onReceive(receiveEvent);    // join i2c bus with address #4
+    Serial.begin(9600);
+    Serial.println("Hello!");    // start serial for output
+
     pinMode(leftM0, OUTPUT);
     pinMode(leftM1, OUTPUT);
     pinMode(rightM0, OUTPUT);
     pinMode(rightM1, OUTPUT);
-    Serial.begin(9600);
-    Serial.println("Hello!");    // start serial for output
 }
 
 void loop()
 {
+    delay(100);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
-void receiveEvent(int howMany)
-{
+void receiveEvent(int howMany){
+    byte left, right;
     while(Wire.available()) // loop through all but the last
     {
         left = Wire.read(); // receive byte as a character
         right = Wire.read();
-        processVals(right, left);
-     }
+    }
+    processVals(left, right);
 }
 
-void processVals(int right, int left){
-    boolean direction_left = bitRead(left, 7);
-    Serial.print("The left one is ");
-    Serial.print(left);
-    Serial.print(". And the direction is ");
-    Serial.println(direction_left);
-    boolean direction_right = bitRead(right, 7);
-    Serial.print("The right one is ");
-    Serial.println(right);
-    Serial.print(". And the direction is ");
-    Serial.println(direction_right);
+void processVals(byte left, byte right){
+    int direction_left = bitRead(left, 7);
+    Serial.print(direction_left);
+    Serial.print(" - ");
+    bitClear(left, 7);
+    Serial.print(left<<1);
+    Serial.print("; ");
+
+    int direction_right = bitRead(right, 7);
+    Serial.print(direction_right);
+    Serial.print(" - ");
+    bitClear(right, 7);
+    Serial.print(right<<1);
+    Serial.println("; ");
     
-    if (direction_right) {    // Move forward
-        analogWrite(rightM1, 0);
-        analogWrite(rightM0, right<<1);
-    } else {
-        analogWrite(rightM0, 0);
-        analogWrite(rightM1, right<<1);
-    }
-    if (direction_left) {    // Move forward
+    if (direction_left == 0) {    // Move left
         analogWrite(leftM1, 0);
         analogWrite(leftM0, left<<1);
     } else {
         analogWrite(leftM0, 0);
         analogWrite(leftM1, left<<1);
+    }
+    if (direction_right == 0) {    // Move rigth
+        analogWrite(rightM1, 0);
+        analogWrite(rightM0, right<<1);
+    } else {
+        analogWrite(rightM0, 0);
+        analogWrite(rightM1, right<<1);
     }
 }
